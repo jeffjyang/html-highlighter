08686c009282622abeb5027fd9132a44386a3fe8
Jeff Yang <jeffjieyang@gmail.com>
2018-02-06 06:31:33 -0800

    document HtmlTag

--- src/HtmlTag.java
+++ src/HtmlTag.java
@@ -11,12 +11,29 @@ public class HtmlTag {
             Arrays.asList("!doctype", "!--", "?xml", "xml", "area", "base",
                           "basefont", "br", "col", "frame", "hr", "img",
                           "input", "link", "meta", "param"));
-    
+    /**
+     * HtmlTag constructor.
+     * 
+     * @param element
+     * 		represents the HTML tag (i.e. "p" or "h2")
+     * @param isOpenTag
+     * 		indicates if the HTML tag is an opening tag or closing tag.
+     * effects: creates an HtmlTag object for the provided tag.
+     */
     public HtmlTag (String element, boolean isOpenTag) {
 	this.element = element;
     	this.isOpenTag = isOpenTag;
     }
         
+    /**
+     * HtmlTag constructor. Will automatically parse the tag for the element 
+     * and whether or not the tag is an opening tag or closing tag. 
+     * 
+     * @param htmlTag
+     * 		full string representation of the HTML tag (i.e. "<p>" or "<head>").
+     * 		requires: htmlTag to be a valid representation of an HTML tag.
+     * effects: creates an HtmlTag object for the provided tag.
+     */
     public HtmlTag (String htmlTag) {
 	if (htmlTag.charAt(1) == '/') {
 	    this.isOpenTag = false;
@@ -31,19 +48,41 @@ public class HtmlTag {
     }
     
 
-    
+    /**
+     * Checks if this tag is an opening tag. Self-closing tags are considered
+     * opening tags.
+     * 
+     * @return true if the tag is an opening tag, false otherwise.
+     */
     public boolean isOpenTag() {
 	return isOpenTag;
     }
     
+    /**
+     * Checks if the HtmlTag is self-closing.
+     * 
+     * @return true if the tag is self closing, false otherwise.
+     */
     public boolean isSelfClosing() {
     	return selfClosingTags.contains(element);
     }
     
+    /**
+     * Gets the element of the HtmlTag.
+     * 
+     * @return the element of the HtmlTag.
+     */
     private String getElement() {
 	return element;
     }
     
+    /**
+     * Checks if this HtmlTag matches the other HtmlTag. Two HtmlTags are considered 
+     * matching if they have the same element and opposite types (i.e. one is an opening
+     * tag and the other is a closing tag).
+     * 
+     * @return true if this HtmlTag matches the other HtmlTag.
+     */
     public boolean matches(HtmlTag other) {
 	if (other.isOpenTag() != isOpenTag) {
 	    return element.equals(other.getElement());
@@ -51,6 +90,11 @@ public class HtmlTag {
 	return false;
     }
     
+    /**
+     * Returns a string representation of the HtmlTag (e.g. "</body>").
+     * 
+     * @return a string representation of the HtmlTag.
+     */
     @Override
     public String toString() {
     	// TODO verify 
@@ -58,6 +102,13 @@ public class HtmlTag {
     	
     }
     
+    /**
+     * Checks if two HtmlTags are equal. Two HtmlTags are considered equal
+     * if they have the same element and same type (i.e. both opening tags or 
+     * both closing tags).
+     * 
+     * @return true if this HtmlTag is equal to o, false otherwise.
+     */
     @Override
     public boolean equals(Object o) {
 	if (o instanceof HtmlTag) {
